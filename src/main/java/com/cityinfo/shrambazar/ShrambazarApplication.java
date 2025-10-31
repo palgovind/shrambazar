fde01fbb51d84d0120ca6ff9846262fb3f96136a
palgovind <pal_govind@yahoo.com>
2025-10-31 12:43:14 +0545

    my commit messages

---
+++ src/main/java/com/cityinfo/shrambazar/ShrambazarApplication.java
@@ -0,0 +1,13 @@
+package com.cityinfo.shrambazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrambazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrambazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cityinfo/shrambazar/ShrambazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityinfo.shrambazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrambazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
